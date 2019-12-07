c60935daee92b24146a174eea2f0603deef9ca36
Jonny Dailey <jonndailey@gmail.com>
2019-12-07 10:48:11 -0500

    Update prime.py
    Resolved the issue slightly. It now only evaluate and prints one time, rather than printing every number in succession.

--- prime.py
+++ prime.py
@@ -15,5 +15,7 @@ if num > 1:
 	for i in range (2, num):
 		if (num % i) == 0:
 			print(num, "is not a prime number")
-	else:
-		print(num, "is a prime number")
+			break
+		else:
+			print(num, "is a prime number")
+			break
